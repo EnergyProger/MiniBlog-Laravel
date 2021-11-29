d7850f2e75fab80ef9bd8f59a56655ea0e0d5648
Eduard Muliar <edikmulyar228@gmail.com>
2021-11-29 15:55:46 +0200

    Added facroty 'PostFactory' and seeder 'PostSeeder'

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,5 +11,5 @@ class Post extends Model
 
     protected $table = 'posts';
 
-    protected $fillable = ['title','description','img','update_at'];
+    protected $fillable = ['title','description','img','updated_at'];
 }
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title'=>$this->faker->sentence,
+            'description'=>$this->faker->text,
+            'img'=>Str::random(20).'.jpg'
+        ];
+    }
+}
